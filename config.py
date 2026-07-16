ca3ea5bf6576d5cd0ee6424ae40c27d1656ff90a
atasamiloglu <samilogluatacelik005@gmail.com>
2026-07-17 01:03:51 +0300

    Add Docker support

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
+import os
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:123456@localhost:5432/todo_db"
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL",
+        "postgresql://postgres:123456@localhost:5432/todo_db"
+    )
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "your-secret-key"
